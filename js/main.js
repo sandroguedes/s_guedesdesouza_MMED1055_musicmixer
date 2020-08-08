439796eedc54f6e9bfbba19cabd2d5112e739514
sandroguedes <59712770+sandroguedes@users.noreply.github.com>
2020-08-08 04:21:32 -0400

    Javascript BugCrush
    Crushed most bugs. The tracks play, loop and come together to form a cacophonous noise that you would hesitate to play even to your worst enemy.

--- js/main.js
+++ js/main.js
@@ -3,24 +3,26 @@ console.log("oh shit, here we go again")
 const   gameBoard = document.querySelector('.icon_board'),
         puzzlePieces = document.querySelectorAll('.icon img'),
         dropZones = document.querySelectorAll('.drop_zone'),
-        zonePieces = document.querySelector('.icon'),
-        audioSample = document.querySelector('.audio_samples');
+         //audioSample = document.querySelector('.audio_samples'),
+        zonePieces = document.querySelector('.icon');
+       
+let audioSample = document.createElement("audio");
 
 
 
 // should play the audio, it remains to be seen if I can actually get it to work D:
+
 const   audioArray = ["Bell2.mp3", "Cinematic1.mp3", "Harp1.mp3", "Morse.mp3", "Percussion01.wav", "Percussion02.wav", "Sample_Synth_03.mp3", "Tech01.wav"];
 let     audioPlaying = 0
 
-function playAudio (ref)
-    {
-        audioSample.src = "./tracks/" +  audioArray[ref];
-        audioSample.load();
-        audioSample.play();
-    }
+//function playAudio (ref)
+  //  {
+    //    audioSample.src = "./tracks/" +  audioArray[ref];
+    //    audioSample.load();
+    //    audioSample.play();
+    //}
 // -----------------------------------------------------------
 // -----------------------------------------------------------
-
 
 
 // allows icons to be dragged around
@@ -46,7 +48,6 @@ function allowDragOver(event)
 // -----------------------------------------------------------
 
 
-
 // allows icons to be dropped in the dropzone
 
 function allowDrop(event)
@@ -59,6 +60,20 @@ function allowDrop(event)
         console.log('dropped an image');
         let droppedImage = event.dataTransfer.getData("draggedImg");
         event.target.appendChild(document.querySelector(`#${droppedImage}`));
+        let n = droppedImage.slice(-1);
+        console.log(n);
+        --n;
+        console.log(n);
+        console.log(audioArray[n]);
+
+        let audioSample = document.createElement("audio");
+        if (event.target.querySelector(`#${droppedImage}`).children.length < 1) {
+            event.target.querySelector(`#${droppedImage}`).appendChild(audioSample);
+            audioSample.src = `tracks/${audioArray[n]}`;
+
+            audioSample.play();
+            audioSample.loop = true;
+        }
     }
 // -----------------------------------------------------------
 // -----------------------------------------------------------
@@ -70,5 +85,5 @@ dropZones.forEach(zone =>
     {
         zone.addEventListener('dragover', allowDragOver);
         zone.addEventListener('drop', allowDrop);
-        zone.addEventListener('drop', playAudio);
+       // zone.addEventListener('drop', playAudio);
     });
